37050be30090c4a5dbe26dde7772ee239745b679
dlatyshev <d_latyshev@ukr.net>
2024-03-20 18:55:22 +0200

    Small changes to factory pattern

--- creational/factory/pizza.py
+++ creational/factory/pizza.py
@@ -3,12 +3,14 @@ from enum import Enum
 
 
 class Size(Enum):
+    """Represents pizza sizes."""
     SMALL = 1
     MEDIUM = 2
     LARGE = 3
 
 
 class Pizza(ABC):
+    """Represents a pizza."""
 
     def __init__(self, size, description: str):
         self.__size = size
@@ -16,10 +18,12 @@ class Pizza(ABC):
 
     @property
     def size(self):
+        """Return the size of the pizza."""
         return self.__size
 
     @property
     def description(self):
+        """Return the description of the pizza."""
         return self.__description
 
     def __str__(self):
@@ -27,24 +31,24 @@ class Pizza(ABC):
 
 
 class CheesePizza(Pizza):
-
+    """Cheese pizza."""
     def __init__(self, size):
         super().__init__(size, "Cheese Pizza")
 
 
 class PepperoniPizza(Pizza):
-
+    """Pepperoni pizza."""
     def __init__(self, size):
         super().__init__(size, "Pepperroni Pizza")
 
 
 class ClamPizza(Pizza):
-
+    """Clam pizza."""
     def __init__(self, size):
         super().__init__(size, "Clam Pizza")
 
 
 class VeganPizza(Pizza):
-
+    """Vegan pizza."""
     def __init__(self, size):
         super().__init__(size, "Vegan Pizza")
--- creational/factory/pizzafactory.py
+++ creational/factory/pizzafactory.py
@@ -1,23 +1,32 @@
+from enum import Enum
 from pizza import Pizza, CheesePizza, ClamPizza, PepperoniPizza, VeganPizza, Size
 
 
+class PizzaType(Enum):
+    CHEESE = "cheese"
+    PEPPERONI = "pepperoni"
+    CLAM = "clam"
+    VEGGIE = "veggie"
+
+
 class SimplePizzaFactory:
+    """A simple pizza factory."""
 
     @staticmethod
-    def create_pizza(size: Size, pizza_type: str) -> Pizza:
+    def create_pizza(size: Size, pizza_type: PizzaType) -> Pizza:
         """Create a pizza of the specified type and size."""
         pizza_types = {
-            "cheese": CheesePizza,
-            "pepperoni": PepperoniPizza,
-            "clam": ClamPizza,
-            "veggie": VeganPizza
+            PizzaType.CHEESE: CheesePizza,
+            PizzaType.PEPPERONI: PepperoniPizza,
+            PizzaType.CLAM: ClamPizza,
+            PizzaType.VEGGIE: VeganPizza
         }
         return pizza_types[pizza_type](size)
 
 
 if __name__ == "__main__":
     factory = SimplePizzaFactory()
-    cheese_pizza = factory.create_pizza(Size.SMALL, "cheese")
-    veggie_pizza = factory.create_pizza(Size.LARGE, "veggie")
+    cheese_pizza = factory.create_pizza(Size.SMALL, PizzaType.CHEESE)
+    veggie_pizza = factory.create_pizza(Size.LARGE, PizzaType.VEGGIE)
     print(cheese_pizza)
     print(veggie_pizza)
